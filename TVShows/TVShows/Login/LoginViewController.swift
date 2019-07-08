abc977cb59073040eb68421aafd9c1ba878aa691
Hrvoje Stefek <hrvoje.stefek@gmail.com>
2019-07-08 18:23:29 +0200

    Sve gotovo.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -13,6 +13,8 @@ class LoginViewController: UIViewController {
     var counter = 0
    
 
+    @IBOutlet weak var mySpinner: UIActivityIndicatorView!
+    
     @IBOutlet weak var myButton: UIButton!
     
     @IBAction func TouchUpInside(_ sender: Any) {
@@ -21,15 +23,22 @@ class LoginViewController: UIViewController {
         if counter < 10{
             Label.text = "Button press counter: " + String(counter)}
         else if counter < 30{
-            Label.text = "You really like this button, don't you? " + String(counter)
+            Label.text = "You really like this button, don't you? \n" + String(counter)
         }
         else{
-            Label.text = "Can you stop doing that? " + String(counter)
+            Label.text = "Can you stop doing that? \n" + String(counter)
+        }
+        
+        if counter%2==1 {
+            mySpinner.startAnimating()
+        }
+        else{
+            mySpinner.stopAnimating()
         }
     }
     @IBOutlet weak var Label: UILabel!{
         didSet{
-            self.Label.text = "What is my purpose? I hope I'm not butter passer..."
+            self.Label.text = "What is my purpose? I hope I'm not a butter passer..."
             self.Label.font = UIFont (name: "Didot", size: 30)
         }
     }
@@ -37,10 +46,15 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
+        mySpinner.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.mySpinner.stopAnimating()
+        }
         myButton.layer.cornerRadius = 10
         myButton.clipsToBounds = true
         myButton.backgroundColor = .darkGray
         myButton.setTitleColor(.blue, for: .normal)
+        myButton.setImage(UIImage(named: "logo")?.withRenderingMode(.alwaysOriginal), for: .normal)
         
         
         // Do any additional setup after loading the view.
