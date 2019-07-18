b412a44556e1beffaf0f9a86bba59494d48ec6b6
Hrvoje Stefek <hrvoje.stefek@gmail.com>
2019-07-18 20:55:34 +0200

    Final final

--- TVShows/TVShows/LoginViewController.swift
+++ TVShows/TVShows/LoginViewController.swift
@@ -9,9 +9,26 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
-
+    
+    private var isChecked: Bool = false
+    
+    // MARK: - Images
+    let checkedImage = UIImage(named: "ic-checkbox-filled")! as UIImage
+    let uncheckedImage = UIImage(named: "ic-checkbox-empty")! as UIImage
+    
     // MARK: - Outlets
     @IBOutlet private weak var logInButton: UIButton!
+    @IBOutlet private weak var checkboxButton: UIButton!
+    
+    @IBAction func checkboxButtonAction() {
+        if isChecked == false{
+            checkboxButton.setImage(checkedImage, for: UIControl.State.normal)
+            isChecked = true
+        } else {
+            checkboxButton.setImage(uncheckedImage, for: UIControl.State.normal)
+            isChecked = false
+        }
+    }
     
     private func configureUI() {
         logInButton.layer.cornerRadius = 5
